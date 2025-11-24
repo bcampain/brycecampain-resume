b6077ca306a6ba54fc2976ed4346d3b893bb27d4
Bryce Campain <12161234+bcampain@users.noreply.github.com>
2025-11-24 15:55:02 -0600

    30 Implement FontAwesome & Fun Mode Icon (#31)

--- BryceCampainResume.Tests/HomePageTests.cs
+++ BryceCampainResume.Tests/HomePageTests.cs
@@ -20,4 +20,28 @@ public class HomePageTests : BunitContext
         Assert.Equal("/Bryce-Campain-Resume-Nov2025.pdf", link.GetAttribute("href"));
         Assert.Equal("Download PDF Résumé", link.TextContent.Trim());
     }
+
+    [Fact]
+    public void FunModeIconIsHiddenByDefault()
+    {
+        var cut = Render<Pages.Home>();
+
+        Assert.Throws<ElementNotFoundException>(() => cut.Find("i.fa-face-laugh-beam"));
+    }
+
+    [Fact]
+    public void FunModeIconAppearsWhenEnabled()
+    {
+        JSInterop.Mode = JSRuntimeMode.Loose;
+        var cut = Render<Pages.Home>();
+
+        var toggle = cut.Find("input[type=checkbox]");
+        toggle.Change(true);
+
+        cut.WaitForAssertion(() =>
+        {
+            var icon = cut.Find("i.fa-face-laugh-beam");
+            Assert.Contains("fa-regular", icon.ClassList);
+        });
+    }
 }
