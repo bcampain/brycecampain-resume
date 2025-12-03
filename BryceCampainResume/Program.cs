77c10e4f335de8a30d8e74a9533787b9d3cc918f
Bryce Campain <12161234+bcampain@users.noreply.github.com>
2025-12-02 19:46:49 -0600

    Update resume content & wire-up automated revision date pull (#36) (#37)
    * Updated content to match new resume draft
    
    * Added functionality to automate pull of resume PDF revision date from blob metadata (#36)

--- BryceCampainResume/Program.cs
+++ BryceCampainResume/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//Test wireup to allow local debugging against the api functions
+var apiBaseAddress = builder.HostEnvironment.IsDevelopment()
+    ? "http://localhost:7071/"
+    : builder.HostEnvironment.BaseAddress;
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseAddress) });
 
 await builder.Build().RunAsync();
